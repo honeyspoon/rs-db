5d35e65ee0229c640b2739ca3437cdc357d2b972
honeyspoon <bobmatt911@gmail.com>
2024-06-06 09:41:57 -0400

    statement vs command

--- src/main.rs
+++ src/main.rs
@@ -5,23 +5,56 @@ fn printhelp() {
     println!("help");
 }
 
+const EXIT_COMMAND: &str = ".exit";
+const HELP_COMMAND: &str = ".help";
+
+enum Statement {
+    Select,
+    Insert,
+}
+
+fn prepare_statment(buffer: &String) -> Option<Statement> {
+    match buffer.trim() {
+        "select" => Some(Statement::Select),
+        "insert" => Some(Statement::Insert),
+        _ => None,
+    }
+}
+
+fn execute_statment(statement: Statement) {
+    match statement {
+        Statement::Insert => {
+            println!("insert");
+        }
+        Statement::Select => {
+            println!("select");
+        }
+    }
+}
+
 fn main() -> io::Result<()> {
     println!("~ rsdb");
-
-    let mut buffer = String::new();
 
     let mut exit = false;
     while !exit {
         print!("> ");
+        let mut buffer = String::new();
         let _ = io::stdout().flush();
-        buffer.clear();
         io::stdin().read_line(&mut buffer)?;
 
-        match buffer.trim() {
-            ".exit" => exit = true,
-            ".help" => printhelp(),
-            _ => println!("unknown command"),
-        }
+        match buffer.chars().next() {
+            Some('.') => match buffer.trim() {
+                EXIT_COMMAND => exit = true,
+                HELP_COMMAND => printhelp(),
+                _ => println!("unknown command"),
+            },
+            Some('\n') => println!("nothing"),
+            Some(_) => match prepare_statment(&buffer) {
+                Some(statement) => execute_statment(statement),
+                None => println!("unsupported command"),
+            },
+            None => println!("error"),
+        };
     }
 
     println!("bye");
