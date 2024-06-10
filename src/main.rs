2e9c13319f511c1e6046b417f516cf4e983b7919
honeyspoon <bobmatt911@gmail.com>
2024-06-10 00:46:51 -0400

    insert and select into mem

--- src/main.rs
+++ src/main.rs
@@ -6,19 +6,83 @@ use serde::{Deserialize, Serialize};
 use rustyline::error::ReadlineError;
 use rustyline::DefaultEditor;
 
+const TABLE_MAX_PAGES: usize = 100;
+const PAGE_SIZE: usize = 4096;
+
+struct Table {
+    nb_rows: u32,
+    pages: Vec<Option<Page>>,
+    row_size: usize,
+}
+
+impl Table {
+    fn new() -> Self {
+        let row_size = bincode::serialized_size(&Row::new(0, "", "")).unwrap() as usize;
+
+        Self {
+            nb_rows: 0,
+            pages: vec![None; TABLE_MAX_PAGES],
+            row_size,
+        }
+    }
+
+    fn get_page_for_row(&self, row_id: u32) -> &Page {
+        let page_id = row_id as usize / TABLE_MAX_PAGES;
+
+        self.pages[page_id].as_ref().unwrap()
+    }
+
+    fn get_page_for_row_mut(&mut self, row_id: u32) -> &mut Page {
+        let page_id = row_id as usize / TABLE_MAX_PAGES;
+        let rows_per_page: usize = PAGE_SIZE / self.row_size;
+
+        if self.pages[page_id].is_none() {
+            let capacity = rows_per_page * self.row_size;
+            self.pages[page_id] = Some(vec![0; capacity]);
+        }
+
+        self.pages[page_id].as_mut().unwrap()
+    }
+
+    fn insert_row(&mut self, row: &Row) {
+        let row_offset = (row.id as usize % TABLE_MAX_PAGES) * self.row_size;
+        let row_bytes = bincode::serialize(&row).unwrap();
+
+        {
+            let page = self.get_page_for_row_mut(row.id);
+            page[row_offset..row_offset + row_bytes.len()].copy_from_slice(&row_bytes);
+        }
+
+        self.nb_rows += 1;
+    }
+
+    fn select_row(&self) {
+        for row_id in 0..self.nb_rows {
+            let page = self.get_page_for_row(row_id);
+            let row_offset = (row_id as usize % TABLE_MAX_PAGES) * self.row_size;
+            let slice_end = row_offset + self.row_size;
+            let slice = &page[row_offset..slice_end];
+            let decoded: Row = bincode::deserialize(slice).unwrap();
+            println!("{:?}", decoded);
+        }
+    }
+}
+
+type Page = Vec<u8>;
+
 #[derive(Serialize, Deserialize)]
 struct Row {
-    id: i32,
+    id: u32,
     username: String,
     email: String,
 }
 
 const COLUMN_USERNAME_SIZE: usize = 32;
-const COLUMN_EMAIL_SIZE: usize = 64;
+const COLUMN_EMAIL_SIZE: usize = 255;
 
 impl Row {
-    fn new(id: i32, username: &str, email: &str) -> Self {
-        Row {
+    fn new(id: u32, username: &str, email: &str) -> Self {
+        Self {
             id,
             username: pad_string(username, COLUMN_USERNAME_SIZE),
             email: pad_string(email, COLUMN_EMAIL_SIZE),
@@ -45,7 +109,7 @@ fn pad_string(input: &str, size: usize) -> String {
 }
 
 enum Statement {
-    Select(Row),
+    Select,
     Insert(Row),
 }
 
@@ -65,27 +129,27 @@ fn prepare_statment(buffer: String) -> Result<Statement, &'static str> {
 
     match parts.as_slice() {
         ["insert", rest @ ..] => parse_insert(rest),
-        ["select", rest @ ..] => Err("not handled yet"),
+        ["select"] => Ok(Statement::Select),
         _ => Err("unknown command"),
     }
 }
 
-fn execute_statment(statement: Statement) -> Result<(), &'static str> {
+fn execute_statment(statement: Statement, table: &mut Table) -> Result<(), &'static str> {
     match statement {
         Statement::Insert(row) => {
-            info!("insert {:?}", row);
+            table.insert_row(&row);
             Ok(())
         }
-        Statement::Select(_) => {
-            println!("select");
+        Statement::Select => {
+            table.select_row();
             Ok(())
         }
     }
 }
 
-fn parse_statement(line: String) -> Result<(), &'static str> {
+fn parse_statement(line: String, table: &mut Table) -> Result<(), &'static str> {
     match prepare_statment(line) {
-        Ok(statement) => execute_statment(statement),
+        Ok(statement) => execute_statment(statement, table),
         Err(err) => Err(err),
     }
 }
@@ -108,10 +172,10 @@ fn parse_command(line: String) -> Result<(), &'static str> {
     }
 }
 
-fn parse_line(line: String) -> Result<(), &'static str> {
+fn parse_line(line: String, table: &mut Table) -> Result<(), &'static str> {
     match line.chars().next() {
         Some('.') => parse_command(line),
-        Some(_) => parse_statement(line),
+        Some(_) => parse_statement(line, table),
         None => Ok(println!("error")),
     }
 }
@@ -127,6 +191,8 @@ fn main() -> rustyline::Result<()> {
         println!("No previous history.");
     }
 
+    let mut table = Table::new();
+
     loop {
         let readline = rl.readline("> ");
 
@@ -136,7 +202,7 @@ fn main() -> rustyline::Result<()> {
                     rl.save_history(hist_file).unwrap();
                 }
 
-                if let Err(err) = parse_line(line) {
+                if let Err(err) = parse_line(line, &mut table) {
                     println!("{}", err);
                 }
             }
